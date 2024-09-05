fd091e7d85767d4a36cbb457d8036352714872f4
Albertoc137 <109186667+Albertoc137@users.noreply.github.com>
2024-09-04 20:28:20 -0700

    otj4

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ document.addEventListener("DOMContentLoaded", function () {
     devFavoritesContent.style.opacity = 1;
   }, 500); // 500 ms delay before starting fade-in
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const meutItems = document.querySelector(".menu-items");
+  // Wait a brief moment, then fade in the image
+  setTimeout(() => {
+    meutItems.style.opacity = 1;
+  }, 500); // 500 ms delay before starting fade-in
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const location = document.querySelector("#location");
+  // Wait a brief moment, then fade in the image
+  setTimeout(() => {
+    location.style.opacity = 1;
+  }, 500); // 500 ms delay before starting fade-in
+});
